c1cd038b1be2faf9d17f5f7c6e84df3ba0934cd9
Pablo Acosta <pablo.acosta@nftstudios.io>
2024-04-09 16:07:30 -0300

    feat: :sparkles: Modified config files for deployment and verification.

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.24;
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
+// Author @pablojacosta
 contract Lock {
     uint public unlockTime;
     address payable public owner;
